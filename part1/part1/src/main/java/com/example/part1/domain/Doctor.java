eb9b60090e40a942dcf31c5b66f9d390857cb6e5
sdr17 <sdr17@student.le.ac.uk>
2025-04-17 00:03:30 +0100

    Created Doctor Setters/getters/Entity

--- part1/part1/src/main/java/com/example/part1/domain/Doctor.java
+++ part1/part1/src/main/java/com/example/part1/domain/Doctor.java
@@ -1,4 +1,42 @@
 package com.example.part1.domain;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Doctor {
+
+    @Id
+    private long id;
+    private String name;
+    private String Specialisation;
+    private String eMail;
+    private String phoneNumber;
+    @OneToMany(orphanRemoval= true, cascade = CascadeType.ALL)
+    private List<Appointments> appointmentsList = new ArrayList<>();
+
+
+    //Setters
+    public void setId(long id) {this.id = id;}
+
+    public void setName(String name) {this.name = name;}
+
+    public void setSpecialisation(String specialisation) {this.Specialisation = specialisation;}
+
+    public void seteMail(String eMail) {this.eMail = eMail;}
+
+    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
+
+    //Getters
+    public long getId() {return id;}
+
+    public String getName() {return name;}
+
+    public String getSpecialisation() {return Specialisation;}
+
+    public String geteMail() {return eMail;}
+
+    public String getPhoneNumber() {return phoneNumber;}
 }
