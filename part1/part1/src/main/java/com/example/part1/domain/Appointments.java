42b9e137af3e5e71f47dcea88ba813c3219084eb
Alex <ae257@student.le.ac.uk>
2025-04-17 13:43:55 +0100

    created appointment and doctor models

--- part1/part1/src/main/java/com/example/part1/domain/Appointments.java
+++ part1/part1/src/main/java/com/example/part1/domain/Appointments.java
@@ -1,4 +1,74 @@
 package com.example.part1.domain;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Appointments {
+
+    @Id
+    private Long id;
+    private Timestamp appointmentDate;
+    private String status;
+    private String notes;
+
+    @ManyToOne
+    private Doctor doctor;
+
+    @ManyToOne
+    private Patient patient;
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Timestamp getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(Timestamp appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+
 }
