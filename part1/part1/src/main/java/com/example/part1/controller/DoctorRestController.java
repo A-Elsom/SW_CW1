06475a655a5cdbbdfbbc5a00f788df839f0d86a4
sdr17 <sdr17@student.le.ac.uk>
2025-04-17 23:07:16 +0100

    did some stuff

--- part1/part1/src/main/java/com/example/part1/controller/DoctorRestController.java
+++ part1/part1/src/main/java/com/example/part1/controller/DoctorRestController.java
@@ -1,4 +1,15 @@
 package com.example.part1.controller;
 
+import com.example.part1.domain.Doctor;
+
+
+@RestController
 public class DoctorRestController {
+
+    @GetMapping("/doctors")
+    public ResponseEntity<List<Doctor>> listAllDoctors(){
+        List<Doctor> doctors = repo.findAll();
+
+        return new ResponseEntity<List<Hotel>>(doctors, HttpStatus.OK);
+    }
 }
