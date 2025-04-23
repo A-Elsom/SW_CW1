0d74bd9e4fb7c228d681258eaa79ef3bb750aa79
Alexander Elsom <ae257@student.le.ac.uk>
2025-04-23 20:34:17 +0100

    created student view model

---
+++ part2/app/src/main/java/com/example/part2/StudentViewModel.java
@@ -0,0 +1,25 @@
+package com.example.part2;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class StudentViewModel extends AndroidViewModel {
+    private StudentRepository repo;
+    private final LiveData<List<Student>> students;
+
+    public StudentViewModel(Application application){
+        super(application);
+        repo = new StudentRepository(application);
+        students = repo.getAllStudents();
+    }
+
+    LiveData<List<Student>> getAllStudents() {return students;}
+
+    public void insert(Student student) {repo.insert(student);}
+
+    public void delete(Student student) {repo.delete(student);}
+}
