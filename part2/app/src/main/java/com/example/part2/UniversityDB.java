6f985266d991a6ec5c17d8dc8cfab020302c9234
Alexander Elsom <ae257@student.le.ac.uk>
2025-04-23 20:13:55 +0100

    added unidb

---
+++ part2/app/src/main/java/com/example/part2/UniversityDB.java
@@ -0,0 +1,30 @@
+package com.example.part2;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Course.class, Student.class}, version = 1, exportSchema = false)
+public abstract class UniversityDB extends RoomDatabase {
+    public abstract CourseDao courseDao();
+    public abstract StudentDao studentDao();
+
+    private static volatile UniversityDB INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    static UniversityDB getDatabase(final Context context){
+        if(INSTANCE == null){
+            synchronized (UniversityDB.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), UniversityDB.class, "uni_DB").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
