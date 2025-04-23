765cfc6cac993fd0e45bc54e2e3a5f0af1fa3907
sdr17 <sdr17@student.le.ac.uk>
2025-04-23 20:49:29 +0100

    Created CourseViewModel

---
+++ part2/app/src/main/java/com/example/part2/CourseViewModel.java
@@ -0,0 +1,26 @@
+package com.example.part2;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class CourseViewModel extends AndroidViewModel {
+
+    private CourseRepository repo;
+    private final LiveData<List<Course>> courses;
+
+    public CourseViewModel(Application application){
+        super(application);
+        repo = new CourseRepository(application);
+        courses = repo.getAllCourses();
+    }
+
+    LiveData<List<Course>> getAllCourses() {return courses;}
+
+    public void insert(Course course) {repo.insert(course);}
+
+    public void delete(Course course) {repo.delete(course);}
+}
