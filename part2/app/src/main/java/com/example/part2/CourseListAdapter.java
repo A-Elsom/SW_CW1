2ecd3cc3769cdfc34280460e129c5297f043f0d8
Alexander Elsom <ae257@student.le.ac.uk>
2025-04-23 23:19:25 +0100

    started work on question 1

---
+++ part2/app/src/main/java/com/example/part2/CourseListAdapter.java
@@ -0,0 +1,37 @@
+package com.example.part2;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CourseListAdapter extends ListAdapter<Course, CourseViewHolder> {
+    public CourseListAdapter(@NotNull DiffUtil.ItemCallback<Course> diffCallback){
+        super(diffCallback);
+    }
+
+    @Override
+    public CourseViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        return CourseViewHolder.Create(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(CourseViewHolder holder, int position){
+        Course current = getItem(position);
+    }
+
+    static class CourseDiff extends DiffUtil.ItemCallback<Course>{
+        @Override
+        public boolean areItemsTheSame(@NonNull Course oldCourse, @NonNull Course newCourse){
+            return oldCourse == newCourse;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Course oldCourse, @NonNull Course newCourse){
+            return oldCourse.getCourseId() == (newCourse.getCourseId());
+        }
+    }
+}
