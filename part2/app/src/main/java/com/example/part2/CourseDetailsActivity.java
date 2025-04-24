9d4b7cd1438194d56d3ffa4e169efc3b9e800119
Alexander Elsom <ae257@student.le.ac.uk>
2025-04-24 12:00:57 +0100

    course activity now displays course code, name and lecturer

--- part2/app/src/main/java/com/example/part2/CourseDetailsActivity.java
+++ part2/app/src/main/java/com/example/part2/CourseDetailsActivity.java
@@ -2,20 +2,39 @@ package com.example.part2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.w3c.dom.Text;
+
 public class CourseDetailsActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstance){
         //load course view
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_course_details);
+        //retieves the TextViews in frontend
+        TextView CourseCodeView = (TextView)findViewById(R.id.courseCode);
+        TextView CourseNameView = (TextView)findViewById(R.id.courseName);
+        TextView CourseLecturerView = (TextView)findViewById(R.id.lecturer_name);
+        //gets the courseViewModel
         CourseViewModel courseViewModel = new ViewModelProvider(this).get(CourseViewModel.class);
+        //debug
         Log.println(Log.INFO, "test", String.valueOf(getIntent().getExtras().getInt("thisCourse")));
-        LiveData<Course> boundCourse = courseViewModel.getCourseById(getIntent().getExtras().getInt("thisCourse"));
+        Log.println(Log.INFO, "test", String.valueOf(courseViewModel.getAllCourses().getValue()));
+        //updates the ui
+        courseViewModel.getCourseById(getIntent().getExtras().getInt("thisCourse")).observe(this,boundCourse -> {
+            String nText = "Name : " + boundCourse.getCourseName();
+            String lText = "Lecturer : " + boundCourse.getLecturerName();
+            CourseCodeView.setText(boundCourse.getCourseCode());
+            CourseNameView.setText(nText);
+            CourseLecturerView.setText(lText);
+        });
 
     }
 }
