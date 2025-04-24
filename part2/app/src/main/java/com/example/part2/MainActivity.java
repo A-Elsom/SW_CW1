cf8648b5071eaeacae6db8dcfd240d8c6b40366a
sdr17 <sdr17@student.le.ac.uk>
2025-04-24 02:00:22 +0100

    completed Q2 of task 2, courses can now be created

--- part2/app/src/main/java/com/example/part2/MainActivity.java
+++ part2/app/src/main/java/com/example/part2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.part2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,12 +29,36 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+    public static final int CREATE_COURSE_REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+//        setSupportActionBar(binding.toolbar);
+
+//        NavController navController = Navigation.findNavController(this, R.id.recyclerview);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//
+//        binding.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAnchorView(R.id.fab)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        //CreateCourse fab action listener
+        FloatingActionButton CCfab = findViewById(R.id.CCfab);
+        CCfab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CreateCourseActivity.class);
+            startActivityForResult(intent, CREATE_COURSE_REQUEST_CODE);
+                });
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         final CourseListAdapter adapter = new CourseListAdapter(new CourseListAdapter.CourseDiff());
@@ -45,6 +71,24 @@ public class MainActivity extends AppCompatActivity {
             adapter.submitList(courses);
         });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CREATE_COURSE_REQUEST_CODE && resultCode == RESULT_OK){
+            String courseCode = data.getStringExtra(CreateCourseActivity.EXTRA_COURSE_CODE);
+            String courseName = data.getStringExtra(CreateCourseActivity.EXTRA_COURSE_NAME);
+            String lecturerName = data.getStringExtra(CreateCourseActivity.EXTRA_LECTURER_NAME);
+
+            Course course = new Course(courseCode, courseName, lecturerName);
+            CourseViewModel vm = new ViewModelProvider(this).get(CourseViewModel.class);
+            vm.insert(course);
+        } else {
+            Toast.makeText(getApplicationContext(),"This Course Was not saved, likley due to empty fields", Toast.LENGTH_LONG).show();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
