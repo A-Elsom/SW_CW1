f857aef77ebad8175ce56113018526a4928236e2
Alexander Elsom <ae257@student.le.ac.uk>
2025-04-24 01:36:23 +0100

    pushing course details Activity

---
+++ part2/app/src/main/java/com/example/part2/CourseDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.part2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CourseDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        //load course view
+        super.onCreate(savedInstance);
+
+
+    }
+}
